0ed65bcc5695b382f4d04a5ab7aa37f8a7ceaef1
az96tv <az96tv@gmail.com>
2022-06-15 12:00:38 +0900

    js change toggle icon

--- 20220608_school cafeteria/js/main.js
+++ 20220608_school cafeteria/js/main.js
@@ -1,19 +1,21 @@
 //show/hide menu: toggle menu
-function toggleMenu(toggleId, navListId){
+const toggleMenu = (toggleId, navListId) => {
 //html -> js
 const toggle = document.getElementById(toggleId);
 const navList = document.getElementById(navListId);
-
-function clickHandler(){
-    //show/hide menu: .show-menu
-    navList.classList.toggle('show-menu');
-}
+const toggleIcon = toggle.getElementsByTagName("i")[0];
     
 if(toggle && navList){
     //toggle click
-    toggle.addEventListener("click", clickHandler);
-    }  
-}
+    toggle.addEventListener("click",() => {
+        //show/hide menu: .show-menu
+        navList.classList.toggle('show-menu');
+        //change toggle icon: bx-menu <-> bx-x
+        toggleIcon.classList.toggle("bx-menu");
+        toggleIcon.classList.toggle("bxs-x-square");
+    });
+ }  
+};
 toggleMenu("nav-toggle", "nav-list");
 
 
