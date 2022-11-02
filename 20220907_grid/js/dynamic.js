1c824700b5f97950829ec50bd1d91efc9a561987
Backseonmi <w2102@e-mirim.hs.kr>
2022-11-02 11:08:11 +0900

    js 이전, 다음 달 변경해도 이벤트 처리하기

--- 20220907_grid/js/dynamic.js
+++ 20220907_grid/js/dynamic.js
@@ -8,6 +8,27 @@ let year = now.getFullYear();
 let month = now.getMonth(); // 0 ~ 11
 month++;
 
+//급식 API, AJAX 급식데이터 가져오기
+//.date-grid-container > .grid-item에 mouseover 이벤트 발생하면, handler를 지정하기
+const handler = (event) => {
+    //handler에서 year, month, date 정보를 가져와서 url 생성하기
+    //console.log("click");
+    let date = event.target.innerHTML;
+    const KEY  = "377ed98d04c94ea089f4463be2eb5e9d"; //급식 api를 받는 곳인 '나이스 교육정보 개방 포털'에 학교 계정으로 로그인해서 발급받은 인증키(탈퇴하면 사용할 수 없어짐)
+    //console.log(KEY);
+    const ATPT_OFCDC_SC_CODE = "B10";       //서울특별시교육청
+    const SD_SCHUL_CODE = "7010569";        //미림여자정보과학고등학교
+    let MLSV_YMD = `${year}${month.toString().padStart(2, "0")}${date.padStart(2, "0")}`;
+    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
+    url += `?KEY=${KEY}`;
+    url += `&Type=json`;
+    url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`;
+    url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`;
+    url += `&MLSV_YMD=${MLSV_YMD}`;
+    // console.log(url);
+    getMenuByAPI(url);
+}
+
 const setCalendar = (year, month) => {
     // 1일이 무슨 요일?
     let firstDate = new Date(year, month - 1, 1);
@@ -51,6 +72,13 @@ const setCalendar = (year, month) => {
     // 1일에 해당하는 div를 grid-column-start: 요일 + 1;
     let firstDateDiv = dateGridContainerDiv.getElementsByClassName("grid-item")[0];
     firstDateDiv.style.gridColumnStart = firstDay + 1;
+
+    //mouseover event 처리
+    let gridItems = dateGridContainerDiv.getElementsByClassName("grid-item");
+    for(let gridItem of gridItems){
+        //console.log(gridItem);
+        gridItem.onmouseover = handler; //mouseover일 때, 이벤트 처리하기
+    }
 }
 
 setCalendar(year, month);
@@ -88,27 +116,6 @@ const initButton = () => {
 }
 
 initButton();
-
-//급식 API, AJAX 급식데이터 가져오기
-//.date-grid-container > .grid-item에 mouseover 이벤트 발생하면, handler를 지정하기
-const handler = (event) => {
-    //handler에서 year, month, date 정보를 가져와서 url 생성하기
-    //console.log("click");
-    let date = event.target.innerHTML;
-    const KEY  = "377ed98d04c94ea089f4463be2eb5e9d"; //급식 api를 받는 곳인 '나이스 교육정보 개방 포털'에 학교 계정으로 로그인해서 발급받은 인증키(탈퇴하면 사용할 수 없어짐)
-    //console.log(KEY);
-    const ATPT_OFCDC_SC_CODE = "B10";       //서울특별시교육청
-    const SD_SCHUL_CODE = "7010569";        //미림여자정보과학고등학교
-    let MLSV_YMD = `${year}${month.toString().padStart(2, "0")}${date.padStart(2, "0")}`;
-    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
-    url += `?KEY=${KEY}`;
-    url += `&Type=json`;
-    url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`;
-    url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`;
-    url += `&MLSV_YMD=${MLSV_YMD}`;
-    // console.log(url);
-    getMenuByAPI(url);
-}
 
 //AJAX(Asynchronous JavaScript And XML)로 url 호출하기
 const getMenuByAPI = (url) => {
@@ -177,11 +184,4 @@ const showMenu = (jsonString) => {
 
 let dateGridContainerDiv = document.getElementsByClassName("date-grid-container")[0];
 let gridItems = dateGridContainerDiv.getElementsByClassName("grid-item");
-
-for(let gridItem of gridItems){
-    //console.log(gridItem);
-    //gridItem.onclick = console.log("click") //실행한 결과를 onclick에 넣는 것
-    //gridItem.onclick = () => {console.log("click")} //onclick을 했을때 실행되고 return하는 것
-    gridItem.onmouseover = handler; //mouseover일 때, 이벤트 처리하기
-}
 
